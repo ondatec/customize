800586834ba1b135f0f69d97a74067bf0b67c7c3
ondatec <45672071+ondatec@users.noreply.github.com>
2018-12-07 12:21:08 -0300

    inc

--- custom.sh
+++ custom.sh
@@ -9,6 +9,16 @@ then
     echo "Just root user can run this script"
     #exit 0;
 fi
+#verifica conecção
+online(){
+if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
+  echo "link ok"
+else
+  echo "Você precisa estar conectado na internet pra rodar esse script;"
+  exit 0;
+fi
+}
+online
 #lista de programas
 PRG=(gimp inkscape geany firefox-esr vim)
 install(){
@@ -18,6 +28,7 @@ install(){
     done
 }
 img() {
+    #funciona apenas no debian 9
     wget https://raw.githubusercontent.com/ondatec/customize/master/wll/odwallyellow.png -O ${GRUBIMG}
 }
 
